3ad9d842b7af54eff707fa1b03b410092e75fa97
swift_gan <939543405@qq.coom>
2024-04-25 18:09:45 +0800

    add pkg alsa-lib

---
+++ packages/alsa-lib/build.sh
@@ -0,0 +1,18 @@
+TERMUX_PKG_HOMEPAGE="https://www.alsa-project.org"
+TERMUX_PKG_DESCRIPTION="An alternative implementation of Linux sound support"
+TERMUX_PKG_LICENSE="LGPL-2.1"
+TERMUX_PKG_MAINTAINER="@termux-pacman"
+TERMUX_PKG_VERSION=1.2.11
+TERMUX_PKG_SRCURL=https://www.alsa-project.org/files/pub/lib/alsa-lib-$TERMUX_PKG_VERSION.tar.bz2
+TERMUX_PKG_SHA256=9f3f2f69b995f9ad37359072fbc69a3a88bfba081fc83e9be30e14662795bb4d
+TERMUX_PKG_DEPENDS="libandroid-sysv-semaphore, libandroid-shmem"
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+--with-tmpdir=$TERMUX_PREFIX_CLASSICAL/tmp
+--disable-static
+--with-versioned=no
+"
+
+termux_step_pre_configure() {
+  LDFLAGS+=" -landroid-sysv-semaphore -landroid-shmem"
+}
